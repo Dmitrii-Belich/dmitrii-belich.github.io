87af90381b78b3e41ac2d1ff17a47260e9ae648a
Dmitrii Belich <dim1999@mail.ru>
2020-06-12 20:51:17 +0300

    style: prettier

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,73 @@
-import {projects, socialNetworks} from '../utils/data.js';
-import Project from '../components/Project.js';
-import Section from '../components/Section.js';
-import MenuLink from '../components/MenuLink.js';
-import Icon from '../components/Icon.js';
-const footerContainer = document.querySelector('.footer__social-networks');
-const profileContainer = document.querySelector('.profile__icons');
+import { projects, socialNetworks } from "../utils/data.js";
+import Project from "../components/Project.js";
+import Section from "../components/Section.js";
+import MenuLink from "../components/MenuLink.js";
+import Icon from "../components/Icon.js";
+const footerContainer = document.querySelector(".footer__social-networks");
+const profileContainer = document.querySelector(".profile__icons");
 
-function setMenu () {
-  const sections = document.querySelectorAll('section')
-  const menu = document.querySelector('.menu');
+function setMenu() {
+  const sections = document.querySelectorAll("section");
+  const menu = document.querySelector(".menu");
   sections.forEach((item) => {
-    const title = item.querySelector('.project-section__title').textContent
-    const element = new MenuLink (title, item.getAttribute('id')).getLink();
-    menu.append(element)
-  })
+    const title = item.querySelector(".project-section__title").textContent;
+    const element = new MenuLink(title, item.getAttribute("id")).getLink();
+    menu.append(element);
+  });
 }
 projects.forEach((item) => {
-  const element = new Section ({title: item.title, subtitle: item.subtitle, id: item.id})
+  const element = new Section({
+    title: item.title,
+    subtitle: item.subtitle,
+    id: item.id,
+  });
   element.setProjects(item.exampls, function callback(item, container) {
-    const project = new Project ({title: item.title, subtitle: item.subtitle, link: item.link, image: item.image})
+    const project = new Project({
+      title: item.title,
+      subtitle: item.subtitle,
+      link: item.link,
+      image: item.image,
+    });
     project.setLinks(item.icons, function callback(item, container) {
-      const icon = new Icon ({link: item.link, icon: item.icon}, 'project-icon').getIcon();
-      container.append(icon)
-    })
-    container.append(project.getProject())
-  })
-  document.querySelector('.content').append(element.getSection())
-})
+      const icon = new Icon(
+        { link: item.link, icon: item.icon },
+        "project-icon"
+      ).getIcon();
+      container.append(icon);
+    });
+    container.append(project.getProject());
+  });
+  document.querySelector(".content").append(element.getSection());
+});
 socialNetworks.forEach((item) => {
-  const element = new Icon ({link: item.link, icon: item.icon}, 'footer-icon').getIcon();
+  const element = new Icon(
+    { link: item.link, icon: item.icon },
+    "footer-icon"
+  ).getIcon();
   footerContainer.append(element);
-})
+});
 socialNetworks.forEach((item) => {
-  const element = new Icon ({link: item.link, icon: item.icon}, 'profile-icon').getIcon();
+  const element = new Icon(
+    { link: item.link, icon: item.icon },
+    "profile-icon"
+  ).getIcon();
   profileContainer.append(element);
-})
-setMenu()
-const Icons = document.querySelectorAll('.social');
-const profileFigure = document.querySelector('.profile__figure');
-const projectFigures = document.querySelectorAll('.project__figure');
+});
+setMenu();
+const Icons = document.querySelectorAll(".social");
+const profileFigure = document.querySelector(".profile__figure");
+const projectFigures = document.querySelectorAll(".project__figure");
 
-if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-  Icons.forEach(item => {
-    item.classList.add('social_type_touch');
-  })
+if (
+  "ontouchstart" in window ||
+  (window.DocumentTouch && document instanceof DocumentTouch)
+) {
+  Icons.forEach((item) => {
+    item.classList.add("social_type_touch");
+  });
 } else {
-  projectFigures.forEach(item => {
-    item.classList.add('project__figure_type_notouch');
-  })
-  profileFigure.classList.add('profile__figure_type_notouch');
-  
+  projectFigures.forEach((item) => {
+    item.classList.add("project__figure_type_notouch");
+  });
+  profileFigure.classList.add("profile__figure_type_notouch");
 }
-
-
--- utils/data.js
+++ utils/data.js
@@ -1,141 +1,149 @@
 export const socialIcons = {
-  vk: './images/vk.png',
-  inst: './images/instagram.png',
-  github: './images/github.png',
-  wats: './images/whatsapp.png',
-  tg: './images/tg.png',
-  slack: './images/slack.png',
-  rekomend: './images/rekomend.svg',
-}
+  vk: "./images/vk.png",
+  inst: "./images/instagram.png",
+  github: "./images/github.png",
+  wats: "./images/whatsapp.png",
+  tg: "./images/tg.png",
+  slack: "./images/slack.png",
+  rekomend: "./images/rekomend.svg",
+};
 export const projects = [
   {
-    title: 'Основные проекты',
-    subtitle: 'Крупные проекты в которых я принимал участие.',
-    id: 'main',
+    title: "Основные проекты",
+    subtitle: "Крупные проекты в которых я принимал участие.",
+    id: "main",
     exampls: [
       {
         title: 'Проект "РакЛечится"',
-        subtitle: 'sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf',
-        link: '',
-        image: './images/raklechitsya.jpg',
+        subtitle:
+          "sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf",
+        link: "",
+        image: "./images/raklechitsya.jpg",
         icons: [
           {
-            name: 'recomendation',
-            link: './rekomendations/raklechitsya.pdf',
+            name: "recomendation",
+            link: "./rekomendations/raklechitsya.pdf",
             icon: socialIcons.rekomend,
           },
           {
-            name: 'code',
-            link: 'https://github.com/Dmitrii-Belich/raklechitsa',
+            name: "code",
+            link: "https://github.com/Dmitrii-Belich/raklechitsa",
             icon: socialIcons.github,
           },
-        ]
+        ],
       },
       {
         title: 'Проект "Место"',
-        subtitle: 'sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf',
-        link: '/mesto',
-        image: './images/mesto.png',
+        subtitle:
+          "sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf",
+        link: "/mesto",
+        image: "./images/mesto.png",
         icons: [
           {
-            name: 'code',
-            link: 'https://github.com/Dmitrii-Belich/mesto',
+            name: "code",
+            link: "https://github.com/Dmitrii-Belich/mesto",
             icon: socialIcons.github,
           },
-        ]
+        ],
       },
       {
         title: 'Проект "Путешествия по России"',
-        subtitle: 'sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf',
-        link: '/russian-travel/',
-        image: './images/across-russia.png',
+        subtitle:
+          "sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf",
+        link: "/russian-travel/",
+        image: "./images/across-russia.png",
         icons: [
           {
-            name: 'code',
-            link: 'https://github.com/Dmitrii-Belich/russian-travel',
+            name: "code",
+            link: "https://github.com/Dmitrii-Belich/russian-travel",
             icon: socialIcons.github,
           },
-        ]
+        ],
       },
       {
         title: 'Проект "Научиться учиться"',
-        subtitle: 'sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf',
-        link: '/how-to-learn/',
-        image: './images/how-to-study.png',
+        subtitle:
+          "sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf",
+        link: "/how-to-learn/",
+        image: "./images/how-to-study.png",
         icons: [
           {
-            name: 'code',
-            link: 'https://github.com/Dmitrii-Belich/how-to-learn',
+            name: "code",
+            link: "https://github.com/Dmitrii-Belich/how-to-learn",
             icon: socialIcons.github,
           },
-        ]
+        ],
       },
-    ]
+    ],
   },
   {
-    title: 'Небольшие проекты',
-    subtitle: 'Не занявшие много времени проекты, однако привнесшие значительный опыт на ранних этапах обучения.',
-    id: 'small',
+    title: "Небольшие проекты",
+    subtitle:
+      "Не занявшие много времени проекты, однако привнесшие значительный опыт на ранних этапах обучения.",
+    id: "small",
     exampls: [
       {
         title: 'Проект "Идеи для прокрастинации"',
-        subtitle: 'sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf',
-        link: '/procrastinate/',
-        image: './images/procrastinate.png',
+        subtitle:
+          "sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf",
+        link: "/procrastinate/",
+        image: "./images/procrastinate.png",
         icons: [
           {
-            name: 'code',
-            link: 'https://github.com/Dmitrii-Belich/procrastinate',
+            name: "code",
+            link: "https://github.com/Dmitrii-Belich/procrastinate",
             icon: socialIcons.github,
           },
-        ]
+        ],
       },
       {
         title: 'Проект "Dappy"',
-        subtitle: 'sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf',
-        link: '/crypto/',
-        image: './images/dappy.svg',
+        subtitle:
+          "sadfsadfsafsadfsa fsafd sa f asfd asd fsa df asdf safsa fd sadf",
+        link: "/crypto/",
+        image: "./images/dappy.svg",
         icons: [
           {
-            name: 'code',
-            link: 'https://github.com/Dmitrii-Belich/crypto',
+            name: "code",
+            link: "https://github.com/Dmitrii-Belich/crypto",
             icon: socialIcons.github,
           },
-        ]
+        ],
       },
-    ]
+    ],
   },
-  ]
+];
 
 export const socialNetworks = [
   {
-    name: 'vk',
-    link: 'https://vk.com/id171896324',
+    name: "vk",
+    link: "https://vk.com/id171896324",
     icon: socialIcons.vk,
   },
   {
-    name: 'inst',
-    link: 'https://www.instagram.com/dmitriy_belich/',
+    name: "inst",
+    link: "https://www.instagram.com/dmitriy_belich/",
     icon: socialIcons.inst,
   },
   {
-    name: 'github',
-    link: 'https://github.com/Dmitrii-Belich',
+    name: "github",
+    link: "https://github.com/Dmitrii-Belich",
     icon: socialIcons.github,
   },
   {
-    name: 'wats',
-    link: 'https://api.whatsapp.com/send?phone=79006351840',
+    name: "wats",
+    link: "https://api.whatsapp.com/send?phone=79006351840",
     icon: socialIcons.wats,
   },
   {
-    name: 'tg',
-    link: 'https://t.me/Diman014',
+    name: "tg",
+    link: "https://t.me/Diman014",
     icon: socialIcons.tg,
   },
   {
-    name: 'slack',
-    link: 'https://vk.com/away.php?utf=1&to=https%3A%2F%2Fjoin.slack.com%2Ft%2Fbelichds%2Fshared_invite%2Fzt-dhlqayr8-WI~TBEZeYWLArgm7Um4gZQ',
+    name: "slack",
+    link:
+      "https://vk.com/away.php?utf=1&to=https%3A%2F%2Fjoin.slack.com%2Ft%2Fbelichds%2Fshared_invite%2Fzt-dhlqayr8-WI~TBEZeYWLArgm7Um4gZQ",
     icon: socialIcons.slack,
   },
-]
+];
